e9e6376fe6b45896d551505d54e6e5915391d57e
Khuzama Shahid <skhuzamashahid@gmail.com>
2020-06-13 11:45:45 +0500

    error fixed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          </p>
+          
         <a
           className="App-link"
           href="https://reactjs.org"
